dd9e39c0b5d5fd3eb2a0ae9dde055ac62192103f
Dimon-Stasyuk <78039077+Dimon-Stasyuk@users.noreply.github.com>
2021-10-17 23:27:36 +0300

    Update ImageGalleryItem.js

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,35 +1,26 @@
 import PropTypes from "prop-types";
-import React from "react";
-// import shortid from "shortid";
+
 import "./ImageGalleryItem.css";
 
-class ImageGalleryItem extends React.Component {
-  render() {
-    return (
-      <>
-        {this.props.images.map(({ webformatURL, largeImageURL, id }) => {
-          return (
-            <li
-              className='ImageGalleryItem'
-              key={id}
-              onClick={() => {
-                this.props.onModalOpen();
-                this.props.ongetImg(largeImageURL);
-              }}>
-              <img
-                src={webformatURL}
-                alt=''
-                className='ImageGalleryItem-image'
-              />
-            </li>
-          );
-        })}
-      </>
-    );
-  }
+export default function ImageGalleryItem({ onModalOpen, ongetImg, images }) {
+  return (
+    <>
+      {images.map(({ webformatURL, largeImageURL, id }) => {
+        return (
+          <li
+            className='ImageGalleryItem'
+            key={id}
+            onClick={() => {
+              onModalOpen();
+              ongetImg(largeImageURL);
+            }}>
+            <img src={webformatURL} alt='' className='ImageGalleryItem-image' />
+          </li>
+        );
+      })}
+    </>
+  );
 }
-
-export default ImageGalleryItem;
 
 ImageGalleryItem.propTypes = {
   images: PropTypes.arrayOf(
